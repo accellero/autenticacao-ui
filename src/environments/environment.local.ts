da357fec92ffc2df42f0ddfb5f7e91bec5043704
phmiranda <pehhagah.1607@gmail.com>
2022-11-25 21:40:23 -0300

    first commit

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,20 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+export const environment = {
+  ApiSso: 'https://localhost:8080/api/realm/developer',
+  ApiServico: 'https://localhost:9000/api/cb/autenticacao',
+  production: false
+};
+
+
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  ApiSso: 'https://api.sso.coderbrains.com.br/api/realm/developer',
+  ApiServico: 'https://api.autenticacao.coderbrains.com.br',
+  production: true
+};
